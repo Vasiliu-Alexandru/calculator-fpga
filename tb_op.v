03036b014f8fc7f1c46a0c333de805b244d36523
sebi-nica <166645810+sebi-nica@users.noreply.github.com>
2024-05-08 21:17:14 +0300

    Update tb_op.v
    to include factorial

--- tb_op.v
+++ tb_op.v
@@ -4,8 +4,8 @@ module tb_op();
 
 reg signed [27:0] a,b;
 reg valid_in,clk,rst;
-wire signed [27:0] rezultat_suma, rezultat_produs, rezultat_diferenta, rezultat_impartire;
-wire valid_out_s, valid_out_d, valid_out_p, valid_out_i, err_suma, err_diferenta, err_produs, err_impartire;
+wire signed [27:0] rezultat_suma, rezultat_produs, rezultat_diferenta, rezultat_impartire, rezultat_factorial;
+wire valid_out_s, valid_out_d, valid_out_p, valid_out_i, err_suma, err_diferenta, err_produs, err_impartire, valid_out_f, err_factorial;
 
 suma s(
 .n1(a),
@@ -47,6 +47,16 @@ impartire i(
 .err(err_impartire),
 .d_out(rezultat_impartire)
 );
+factorial f(
+//.n1(a),
+.n(b),
+.valid_in(valid_in),
+.clk(clk),
+.rst(rst),
+.valid_out(valid_out_f),
+.ovrflow(err_factorial),
+.d_out(rezultat_factorial)
+);
 
 
 initial begin
@@ -67,7 +77,7 @@ rst=1; //out of reset
 repeat(50)@(posedge clk);
 valid_in=1;
 a=412;
-b=3534;
+b=6;
 repeat(1)@(posedge clk);
 valid_in=0;
 
@@ -91,7 +101,7 @@ valid_in=0;
 repeat(50)@(posedge clk);
 valid_in=1;
 a=-364526534;
-b=-5346;
+b=-5;
 repeat(1)@(posedge clk);
 valid_in=0;
 
@@ -105,9 +115,18 @@ valid_in=0;
 repeat(50)@(posedge clk);
 valid_in=1;
 a=168;
-b=13;
+b=25;
 repeat(1)@(posedge clk);
 valid_in=0;
+
+repeat(50)@(posedge clk);
+valid_in=1;
+a=168;
+b=3;
+repeat(1)@(posedge clk);
+valid_in=0;
+
+
 
 end
 
