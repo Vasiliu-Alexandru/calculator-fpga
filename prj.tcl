3e4cd9726f357ce0d0a04a1d947cd83feeeb05d7
Vasiliu-Alexandru <166641942+Vasiliu-Alexandru@users.noreply.github.com>
2024-06-24 11:40:23 +0300

    Update prj.tcl

--- prj.tcl
+++ prj.tcl
@@ -3,6 +3,9 @@ project_new example1 -overwrite
 set_global_assignment -name FAMILY "Cyclone IV E"
 set_global_assignment -name DEVICE EP4CE10E22C8
 
+set_global_assignment -name CYCLONEII_RESERVE_NCEO_AFTER_CONFIGURATION "USE AS REGULAR IO"
+
+set_global_assignment -name VERILOG_FILE toplevel.v
 set_global_assignment -name VERILOG_FILE hex2segments.v
 set_global_assignment -name VERILOG_FILE PS2_receiver.v
 set_global_assignment -name VERILOG_FILE formnumbers.v
@@ -10,6 +13,10 @@ set_global_assignment -name VERILOG_FILE clock_sync.v
 set_global_assignment -name VERILOG_FILE produs.v
 set_global_assignment -name VERILOG_FILE impartire.v
 set_global_assignment -name VERILOG_FILE radical.v
+set_global_assignment -name VERILOG_FILE pll_25.v
+set_global_assignment -name VERILOG_FILE vga_display.v
+set_global_assignment -name SDC_FILE example1.sdc
+
 
 set_global_assignment -name TOP_LEVEL_ENTITY toplevel
 
@@ -58,6 +65,13 @@ set_location_assignment PIN_84 -to a_t[3]
 set_location_assignment PIN_85 -to a_t[2]
 set_location_assignment PIN_86 -to a_t[1]
 set_location_assignment PIN_87 -to sign_out
+
+set_location_assignment PIN_101 -to hsync
+set_location_assignment PIN_103 -to vsync
+set_location_assignment PIN_106 -to red
+set_location_assignment PIN_105 -to green
+set_location_assignment PIN_104 -to blue
+
 
 
 load_package flow
