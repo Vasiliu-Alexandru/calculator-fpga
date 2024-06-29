f5fc20acde2f322a1633d0ddbfb56b3769246a67
Vasiliu-Alexandru <166641942+Vasiliu-Alexandru@users.noreply.github.com>
2024-06-29 21:01:49 +0300

    Update bin_2_bcd(4 digits).v

--- bin_2_bcd(4 digits).v
+++ bin_2_bcd(4 digits).v
@@ -18,12 +18,12 @@ integer i;
 	
 always @(*) begin
     bcd=0;		 	
-    for (i=0;i<14;i=i+1) begin					//Iterate once for each bit in input number
-        if (bcd[3:0] >= 5) bcd[3:0] = bcd[3:0] + 3;		//If any BCD digit is >= 5, add three
+    for (i=0;i<14;i=i+1) begin					
+        if (bcd[3:0] >= 5) bcd[3:0] = bcd[3:0] + 3;		
 	if (bcd[7:4] >= 5) bcd[7:4] = bcd[7:4] + 3;
 	if (bcd[11:8] >= 5) bcd[11:8] = bcd[11:8] + 3;
 	if (bcd[15:12] >= 5) bcd[15:12] = bcd[15:12] + 3;
-	bcd = {bcd[14:0],d_in[13-i]};				//Shift one bit, and shift in proper bit from input 
+	bcd = {bcd[14:0],d_in[13-i]};				
     end
 end
 	
